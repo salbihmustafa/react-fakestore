4b9daea38eefcce0a8a770958e22b12ba1515d5e
salbihmustafa <salbih.mustafa@certegy.com>
2021-11-16 14:51:23 -0500

    display items with overflow y set to auto

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -5,7 +5,7 @@ const ProductCard = (props) => {
   return (
     <div className="card-wrapper">
       <div className="image-section">
-        {<img src={props.image} width={100} height={100} />}
+        {<img src={props.image} width={135} height={135} alt="test" />}
       </div>
       <div className="section2">Test2</div>
       <div className="section3">Test3</div>
--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./ProductList.css";
 import ProductCard from "./ProductCard";
 import FakeStoreApi from "../FakeStoreAPI/FakeStoreApi";
+import axios from "axios";
 
 const ProductSection = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
+    const source = axios.CancelToken.source();
+
     const fetchData = async () => {
       try{
         const response = await FakeStoreApi.get("/products");
@@ -15,8 +18,15 @@ const ProductSection = () => {
       }catch(err){
         console.log("error", err);
       }
+      
+      
     }
     fetchData();
+    return () => {
+      source.cancel(); //this will prevent memory leaking
+      console.log("Clean up");
+    }
+    
   }, []);
   
   const displayProducts = products.map(({id, image}) => {
